e28b5cb0d947dcf5e8b2cf0413fdf5e00aa1f52c
typhonrt <support@typhonrt.org>
2017-03-13 21:47:01 -0700

    added test source watcher.

--- src/plugin.js
+++ src/plugin.js
@@ -15,132 +15,222 @@ export function onComplete(ev)
 
    const config = eventbus.triggerSync('tjsdoc:data:config:get');
 
-   eventbus.trigger('log:info:time', `tjsdoc-plugin-watcher - watching source globs: ${
-    JSON.stringify(config._sourceGlobs)}`);
-
    const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
       prompt: '[32mTJSDoc>[0m '
    });
 
-   let watcher;
+   let sourceWatcher, testWatcher;
    let watcherCloseFunction;
    let promptVisible = false;
 
-   // Watch all source files.
-   gaze(config._sourceGlobs, { debounceDelay: 500 }, (err, newWatcher) =>
+   if (config._sourceGlobs)
    {
-      watcher = newWatcher;
+      eventbus.trigger('log:info:time', `tjsdoc-plugin-watcher - watching source globs: ${
+       JSON.stringify(config._sourceGlobs)}`);
 
-      // On file changed
-      watcher.on('changed', (filePath) =>
+      // Watch all source files.
+      gaze(config._sourceGlobs, { debounceDelay: 500 }, (err, watcher) =>
       {
-         if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
+         sourceWatcher = watcher;
 
-         eventbus.trigger('log:info:time', `changed: ${filePath}`);
-
-         try
+         // On file changed
+         sourceWatcher.on('changed', (filePath) =>
          {
-            const result = eventbus.triggerSync('tjsdoc:system:generate:file:doc:data', filePath);
+            if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
 
-            if (result)
+            eventbus.trigger('log:info:time', `changed: ${filePath}`);
+            eventbus.trigger('tjsdoc:system:watcher:file:changed', { type: 'source', filePath });
+         });
+
+         // On file added
+         sourceWatcher.on('added', (filePath) =>
+         {
+            if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
+
+            eventbus.trigger('log:info:time', `addition: ${filePath}`);
+            eventbus.trigger('tjsdoc:system:watcher:file:added', { type: 'source', filePath });
+         });
+
+         // On file deleted
+         sourceWatcher.on('deleted', (filePath) =>
+         {
+            if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
+
+            eventbus.trigger('log:info:time', `deletion: ${filePath}`);
+            eventbus.trigger('tjsdoc:system:watcher:file:deleted', { type: 'source', filePath });
+         });
+
+         sourceWatcher.on('end', () =>
+         {
+            if (typeof watcherCloseFunction === 'function')
             {
-               eventbus.trigger('log:info:time', `docData: ${JSON.stringify(result.docData)}`);
+               const closeFunction = watcherCloseFunction;
+               watcherCloseFunction = void 0;
+               setImmediate(closeFunction);
             }
-            else
+         });
+
+         // Get watched files with relative paths
+         const files = sourceWatcher.relative();
+
+         eventbus.trigger('log:info:time', `tjsdoc-plugin-watcher - watching source files: ${JSON.stringify(files)}`);
+      });
+   }
+
+   if (config.test && config.test._sourceGlobs)
+   {
+      eventbus.trigger('log:info:time', `tjsdoc-plugin-watcher - watching test globs: ${
+       JSON.stringify(config.test._sourceGlobs)}`);
+
+      // Watch all test files.
+      gaze(config.test._sourceGlobs, { debounceDelay: 500 }, (err, watcher) =>
+      {
+         testWatcher = watcher;
+
+         // On file changed
+         testWatcher.on('changed', (filePath) =>
+         {
+            if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
+
+            eventbus.trigger('log:info:time', `changed: ${filePath}`);
+            eventbus.trigger('tjsdoc:system:watcher:file:changed', { type: 'source', filePath });
+         });
+
+         // On file added
+         testWatcher.on('added', (filePath) =>
+         {
+            if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
+
+            eventbus.trigger('log:info:time', `addition: ${filePath}`);
+            eventbus.trigger('tjsdoc:system:watcher:file:added', { type: 'source', filePath });
+         });
+
+         // On file deleted
+         testWatcher.on('deleted', (filePath) =>
+         {
+            if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
+
+            eventbus.trigger('log:info:time', `deletion: ${filePath}`);
+            eventbus.trigger('tjsdoc:system:watcher:file:deleted', { type: 'source', filePath });
+         });
+
+         testWatcher.on('end', () =>
+         {
+            if (typeof watcherCloseFunction === 'function' && sourceWatcher)
             {
-               eventbus.trigger('log:warn:time', `Failed doc data generation: ${filePath}`);
+               sourceWatcher.close();
             }
-         }
-         catch (err)
-         {
-            eventbus.trigger('log:warn:time', `Failed doc data generation: ${filePath}`, err);
-         }
+         });
+
+         // Get watched files with relative paths
+         const files = testWatcher.relative();
+
+         eventbus.trigger('log:info:time', `tjsdoc-plugin-watcher - watching test files: ${JSON.stringify(files)}`);
       });
+   }
 
-      // On file added
-      watcher.on('added', (filePath) =>
-      {
-         if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
-
-         eventbus.trigger('log:info:time', `addition: ${filePath}`);
-      });
-
-      // On file deleted
-      watcher.on('deleted', (filePath) =>
-      {
-         if (promptVisible) { eventbus.trigger('log:info:raw', ''); }
-
-         eventbus.trigger('log:info:time', `deletion: ${filePath}`);
-      });
-
-      watcher.on('end', () =>
-      {
-         if (typeof watcherCloseFunction === 'function')
-         {
-            const closeFunction = watcherCloseFunction;
-            watcherCloseFunction = void 0;
-            setImmediate(closeFunction);
-         }
-      });
-
-      // Get watched files with relative paths
-      const files = watcher.relative();
-
-      eventbus.trigger('log:info:time', `tjsdoc-plugin-watcher - watching files: ${JSON.stringify(files)}`);
-   });
-
-   process.on('SIGINT', () =>
+   if (config._sourceGlobs || (config.test && config.test._sourceGlobs))
    {
-      eventbus.trigger('log:warn:time', 'Received SIGINT. Shutting down.');
-
-      if (rl) { rl.close(); }
-      if (watcher) { watcher.close(); }
-
-      setImmediate(() => process.exit(0));
-   });
-
-   // Set terminal readline loop waiting for the user to type in the commands: `restart` or `exit`.
-
-   rl.on('line', (line) =>
-   {
-      switch (line.trim())
+      process.on('SIGINT', () =>
       {
-         case 'exit':
-            promptVisible = false;
-            watcher.close();
-            rl.close();
-            process.removeAllListeners('SIGINT');
-            setImmediate(() => process.exit(0));
-            break;
+         eventbus.trigger('log:warn:time', 'Received SIGINT. Shutting down.');
 
-         case 'globs':
-            promptVisible = false;
-            eventbus.trigger('log:info:time', `watching globs: ${JSON.stringify(config._sourceGlobs)}`);
-            break;
+         if (rl) { rl.close(); }
+         if (sourceWatcher) { sourceWatcher.close(); }
+         if (testWatcher) { testWatcher.close(); }
 
-         case 'regen':
-         case 'regenerate':
-            promptVisible = false;
-            setImmediate(() =>
-            {
-               watcherCloseFunction = () => { eventbus.trigger('tjsdoc:system:regenerate:all:docs'); };
-               process.removeAllListeners('SIGINT');
-               watcher.close();
+         setImmediate(() => process.exit(0));
+      });
+
+      // Set terminal readline loop waiting for the user to type in the commands: `restart` or `exit`.
+
+      rl.on('line', (line) =>
+      {
+         switch (line.trim())
+         {
+            case 'exit':
+               promptVisible = false;
+
                rl.close();
-            });
-            break;
+               if (sourceWatcher) { sourceWatcher.close(); }
+               if (testWatcher) { testWatcher.close(); }
 
-         case 'watching':
-            promptVisible = false;
-            if (watcher) { eventbus.trigger('log:info:time', `watching files: ${JSON.stringify(watcher.relative())}`); }
-            break;
+               eventbus.trigger('tjsdoc:system:watcher:stopped');
 
-         default:
-            promptVisible = true;
-            rl.prompt();
-            break;
-      }
-   });
+               process.removeAllListeners('SIGINT');
+
+               setImmediate(() => process.exit(0));
+               break;
+
+            case 'globs':
+               promptVisible = false;
+
+               if (config._sourceGlobs)
+               {
+                  eventbus.trigger('log:info:time', `watching source globs: ${JSON.stringify(config._sourceGlobs)}`);
+               }
+
+               if (config.test && config.test._sourceGlobs)
+               {
+                  eventbus.trigger('log:info:time', `watching test globs: ${JSON.stringify(config.test._sourceGlobs)}`);
+               }
+               break;
+
+            case 'regen':
+            case 'regenerate':
+               promptVisible = false;
+
+               setImmediate(() =>
+               {
+                  watcherCloseFunction = () =>
+                  {
+                     eventbus.trigger('tjsdoc:system:watcher:stopped');
+                     eventbus.trigger('tjsdoc:system:regenerate:all:docs');
+                  };
+
+                  process.removeAllListeners('SIGINT');
+
+                  // If `testWatcher` exists it will close `sourceWatcher` otherwise
+                  if (testWatcher)
+                  {
+                     testWatcher.close();
+                  }
+                  else if (sourceWatcher)
+                  {
+                     sourceWatcher.close();
+                  }
+
+                  rl.close();
+               });
+               break;
+
+            case 'watching':
+               promptVisible = false;
+
+               if (sourceWatcher)
+               {
+                  eventbus.trigger('log:info:time', `watching source files: ${JSON.stringify(sourceWatcher.relative())}`);
+               }
+
+               if (testWatcher)
+               {
+                  eventbus.trigger('log:info:time', `watching test files: ${JSON.stringify(testWatcher.relative())}`);
+               }
+               break;
+
+            default:
+               promptVisible = true;
+               rl.prompt();
+               break;
+         }
+      });
+
+      eventbus.trigger('tjsdoc:system:watcher:started');
+   }
+   else
+   {
+      eventbus.trigger('log:info:time', 'tjsdoc-plugin-watcher: no main source or tests to watch.');
+   }
 }
